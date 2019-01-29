c7b196b3b2472838d586ff868016f94bf03bf993
tianhaojie <1689177720@qq.com>
2019-01-29 23:49:09 +0800

    水票

--- src/stores/orderConfirm.js
+++ src/stores/orderConfirm.js
@@ -1,5 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 const CHOOSETICKET = 'CHOOSETICKET'
+const GETCHOOSELIST = 'GETCHOOSELIST'
+
+const mytickets = [{ name: '新人首单立减', money: 20 },
+  { name: '新人首单立减', money: 30 },
+  { name: '新人首单立减', money: 20 },
+  { name: '新人首单立减', money: 20 },
+  { name: '新人首单立减', money: 20 }];
 
 const initState = {
   info: {},
@@ -14,8 +21,19 @@ export const orderConfirm = (state = initState, action) => {
         ...state,
         ...action.payload,
       }
+    case GETCHOOSELIST:
+      return {
+        ...state,
+        tickets: mytickets,
+      }
     default:
       return state
+  }
+}
+export const getTicketsList = (params) => {
+  return {
+    payload: params,
+    type: GETCHOOSELIST,
   }
 }
 export const chooseTicket = (params) => {
