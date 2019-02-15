a2c681fe8732b4a6bedb9952141dc6732a63645b
love1234 <345841634@qq.com>
2019-02-15 15:03:15 +0800

    xx

--- src/pages/common-components/skeleton/entry.jsx
+++ src/pages/common-components/skeleton/entry.jsx
@@ -8,7 +8,7 @@ export default class EntrySk extends React.PureComponent {
     return (
       <div className={styles.entry}>
         {
-          Array.from({ length: 10 }, (v, i) => i).map(v => (
+          Array.from({ length: 5 }, (v, i) => i).map(v => (
             <div key={v} className={styles.item}>
               <div className={styles.circle} />
               <div className={styles.desc} />
